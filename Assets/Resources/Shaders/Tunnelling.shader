7669467c7b18ffe6953efd29e5382edba03a3162
Harvey Ball <me@harveyball.com>
2017-11-12 08:16:26 +0000

    add optional cage skybox texture
    A CubeMap texture can now be provided to generate
    a cage skybox for the reference view.

--- Assets/Resources/Shaders/Tunnelling.shader
+++ Assets/Resources/Shaders/Tunnelling.shader
@@ -1,54 +1,66 @@
-﻿Shader "Hidden/Tunnelling" {
-	Properties {
-		_MainTex ("Texture", 2D) = "white" {}
-		_AV ("Angular Velocity", Float) = 0
-		_Feather ("Feather", Float) = 0.1
-	}
-	SubShader {
-		// No culling or depth
-		Cull Off ZWrite Off ZTest Always
+﻿// Upgrade NOTE: replaced 'mul(UNITY_MATRIX_MVP,*)' with 'UnityObjectToClipPos(*)'
 
-		Pass {
-			CGPROGRAM
-			#pragma vertex vert
-			#pragma fragment frag
-			
-			#include "UnityCG.cginc"
+Shader "Hidden/Tunnelling" {
+    Properties {
+        _MainTex ("Texture", 2D) = "white" {}
+        _AV ("Angular Velocity", Float) = 0
+        _Feather ("Feather", Float) = 0.1
+        _CageCubeMap ("Cage CubeMap", Cube) = "" {}
+    }
+    SubShader {
+        // No culling or depth
+        Cull Off ZWrite Off ZTest Always
 
-			struct appdata {
-				float4 vertex : POSITION;
-				float2 uv : TEXCOORD0;
-			};
+        Pass {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            
+            #include "UnityCG.cginc"
 
-			struct v2f {
-				float2 uv : TEXCOORD0;
-				float4 vertex : SV_POSITION;
-			};
+            struct appdata {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
 
-			v2f vert (appdata v) {
-				v2f o;
-				o.vertex = mul(UNITY_MATRIX_MVP, v.vertex);
-				o.uv = v.uv;
-				return o;
-			}
-			
-			sampler2D _MainTex;
-			float4 _MainTex_ST;
-			float _AV;
-			float _Feather;
+            struct v2f {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
 
-			fixed4 frag (v2f i) : SV_Target {
-				float2 uv = UnityStereoScreenSpaceUVAdjust(i.uv, _MainTex_ST);
-				fixed4 col = tex2D(_MainTex, uv);
+            v2f vert (appdata v) {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = v.uv;
+                return o;
+            }
+            
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
+            float _AV;
+            float _Feather;
+            samplerCUBE _CageCubeMap;
+            float4x4 _EyeProjection[2];
+            float4x4 _EyeToWorld[2];
 
-				float2 coords = (i.uv - 0.5) * 2;
-				float radius = length(coords) / 1.414214;
-				float avMin = (1 - _AV) - _Feather;
-				float avMax = (1 - _AV) + _Feather;
-				float t = saturate((radius - avMin) / (avMax - avMin));
-				return lerp(col, fixed4(0,0,0,0), t);
-			}
-			ENDCG
-		}
-	}
+            fixed4 frag (v2f i) : SV_Target {
+                float2 uv = UnityStereoScreenSpaceUVAdjust(i.uv, _MainTex_ST);
+                fixed4 col = tex2D(_MainTex, uv);
+                fixed4 fadeCol = fixed4(0,0,0,0);
+                float2 coords = (i.uv - 0.5) * 2;
+                float4 viewPos = mul(_EyeProjection[unity_StereoEyeIndex], float4(coords, 0, 1));
+                viewPos.xyz /- viewPos.w;
+
+                float radius = length(viewPos.xy / (_ScreenParams.xy / 2)) / 2;
+                float avMin = (1 - _AV) - _Feather;
+                float avMax = (1 - _AV) + _Feather;
+                float t = saturate((radius - avMin) / (avMax - avMin));
+
+                float rayDir = mul(_EyeToWorld[unity_StereoEyeIndex], viewPos).xyz;
+                half4 cageData = texCUBE(_CageCubeMap, normalize(rayDir));
+                return lerp(col, cageData * fadeCol, t);
+            }
+            ENDCG
+        }
+    }
 }
--- Assets/Scripts/ImageEffects/Tunnelling.cs
+++ Assets/Scripts/ImageEffects/Tunnelling.cs
@@ -1,122 +1,187 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-namespace Sigtrap.ImageEffects {
-	public class Tunnelling : MonoBehaviour {
-		#region Public Fields
-		[Header("Angular Velocity")]
-		/// <summary>
-		/// Angular velocity calculated for this Transform. DO NOT USE HMD!
-		/// </summary>
-		[Tooltip("Angular velocity calculated for this Transform.\nDO NOT USE HMD!")]
-		public Transform refTransform;
+namespace Sigtrap.ImageEffects
+{
+    public class Tunnelling : MonoBehaviour
+    {
+        #region Public Fields
+        [Header("Angular Velocity")]
+        /// <summary>
+        /// Angular velocity calculated for this Transform. DO NOT USE HMD!
+        /// </summary>
+        [Tooltip("Angular velocity calculated for this Transform.\nDO NOT USE HMD!")]
+        public Transform refTransform;
 
-		/// <summary>
-		/// Below this angular velocity, effect will not kick in. Degrees per second
-		/// </summary>
-		[Tooltip("Below this angular velocity, effect will not kick in.\nDegrees per second")]
-		public float minAngVel = 0f;
+        /// <summary>
+        /// Below this angular velocity, effect will not kick in. Degrees per second
+        /// </summary>
+        [Tooltip("Below this angular velocity, effect will not kick in.\nDegrees per second")]
+        public float minAngVel = 0f;
 
-		/// <summary>
-		/// At/above this angular velocity, effect will be maxed out. Degrees per second
-		/// </summary>
-		[Tooltip("At/above this angular velocity, effect will be maxed out.\nDegrees per second")]
-		public float maxAngVel = 180f;
+        /// <summary>
+        /// At/above this angular velocity, effect will be maxed out. Degrees per second
+        /// </summary>
+        [Tooltip("At/above this angular velocity, effect will be maxed out.\nDegrees per second")]
+        public float maxAngVel = 180f;
 
-		/// <summary>
-		/// Below this speed, effect will not kick in.
-		/// </summary>
-		[Tooltip("Below this speed, effect will not kick in.")]
-		public float minSpeed = 0f;
+        /// <summary>
+        /// Below this speed, effect will not kick in.
+        /// </summary>
+        [Tooltip("Below this speed, effect will not kick in.")]
+        public float minSpeed = 0f;
 
-		/// <summary>
-		/// At/above this speed, effect will be maxed out.
-		/// </summary>
-		[Tooltip("At/above this speed, effect will be maxed out.\nSet negative for no effect.")]
-		public float maxSpeed = -1f;
+        /// <summary>
+        /// At/above this speed, effect will be maxed out.
+        /// </summary>
+        [Tooltip("At/above this speed, effect will be maxed out.\nSet negative for no effect.")]
+        public float maxSpeed = -1f;
 
-		[Header("Effect Settings")]
-		/// <summary>
-		/// Screen coverage at max angular velocity.
-		/// </summary>
-		[Range(0f,1f)][Tooltip("Screen coverage at max angular velocity.\n(1-this) is radius of visible area at max effect (screen space).")]
-		public float maxEffect = 0.75f;
+        [Header("Effect Settings")]
+        /// <summary>
+        /// Screen coverage at max angular velocity.
+        /// </summary>
+        [Range(0f, 1f)]
+        [Tooltip("Screen coverage at max angular velocity.\n(1-this) is radius of visible area at max effect (screen space).")]
+        public float maxEffect = 0.75f;
 
-		/// <summary>
-		/// Feather around cut-off as fraction of screen.
-		/// </summary>
-		[Range(0f, 0.5f)][Tooltip("Feather around cut-off as fraction of screen.")]
-		public float feather = 0.1f;
+        /// <summary>
+        /// Feather around cut-off as fraction of screen.
+        /// </summary>
+        [Range(0f, 0.5f)]
+        [Tooltip("Feather around cut-off as fraction of screen.")]
+        public float feather = 0.1f;
 
-		/// <summary>
-		/// Smooth out radius over time. 0 for no smoothing.
-		/// </summary>
-		[Tooltip("Smooth out radius over time. 0 for no smoothing.")]
-		public float smoothTime = 0.15f;
-		#endregion
+        /// <summary>
+        /// Smooth out radius over time. 0 for no smoothing.
+        /// </summary>
+        [Tooltip("Smooth out radius over time. 0 for no smoothing.")]
+        public float smoothTime = 0.15f;
 
-		#region Smoothing
-		private float _avSlew;
-		private float _av;
-		#endregion
+        /// <summary>
+        /// An optional CubeMap texture for the cage skybox.
+        /// </summary>
+        [Tooltip("An optional CubeMap texture for the cage skybox.")]
+        public Texture cageSkybox;
+        #endregion
 
-		#region Shader property IDs
-		private int _propAV;
-		private int _propFeather;
-		#endregion
+        #region Smoothing
+        private float _avSlew;
+        private float _av;
+        #endregion
 
-		#region Misc Fields
-		private Vector3 _lastFwd;
-		private Vector3 _lastPos;
-		private Material _m;
-		#endregion
+        #region Shader property IDs
+        private int _propAV;
+        private int _propFeather;
+        private int _propCageCubeMap;
+        #endregion
 
-		#region Messages
-		void Awake () {
-			_m = new Material(Shader.Find("Hidden/Tunnelling"));
+        #region Misc Fields
+        private Vector3 _lastFwd;
+        private Vector3 _lastPos;
+        private Material _m;
+        private Camera _camera;
+        #endregion
 
-			if (refTransform == null){
-				refTransform = transform;
-			}
+        #region Messages
+        void Awake()
+        {
+            _m = new Material(Shader.Find("Hidden/Tunnelling"));
+            _camera = Camera.main;
+            if (refTransform == null)
+            {
+                refTransform = transform;
+            }
 
-			_propAV = Shader.PropertyToID("_AV");
-			_propFeather = Shader.PropertyToID("_Feather");
-		}
+            _propAV = Shader.PropertyToID("_AV");
+            _propFeather = Shader.PropertyToID("_Feather");
+            _propCageCubeMap = Shader.PropertyToID("_CageCubeMap");
+            SetCageSkyboxTexture();
+        }
 
-		void Update(){
-			Vector3 fwd = refTransform.forward;
-			float av = Vector3.Angle(_lastFwd, fwd) / Time.deltaTime;
-			av = (av - minAngVel) / (maxAngVel - minAngVel);
+        void SetCageSkyboxTexture()
+        {
+            //If not cage skybox texture is provided, create a temporary white texture for blending
+            if (cageSkybox == null)
+            {
+                Cubemap tempTexture = new Cubemap(1, TextureFormat.ARGB32, false);
+                tempTexture.SetPixel(CubemapFace.NegativeX, 0, 0, Color.white);
+                tempTexture.SetPixel(CubemapFace.NegativeY, 0, 0, Color.white);
+                tempTexture.SetPixel(CubemapFace.NegativeZ, 0, 0, Color.white);
+                tempTexture.SetPixel(CubemapFace.PositiveX, 0, 0, Color.white);
+                tempTexture.SetPixel(CubemapFace.PositiveY, 0, 0, Color.white);
+                tempTexture.SetPixel(CubemapFace.PositiveZ, 0, 0, Color.white);
+                cageSkybox = tempTexture;
+            }
+            _m.SetTexture(_propCageCubeMap, cageSkybox);
+        }
 
-			Vector3 pos = refTransform.position;
+        void Update()
+        {
+            Vector3 fwd = refTransform.forward;
+            float av = Vector3.Angle(_lastFwd, fwd) / Time.deltaTime;
+            av = (av - minAngVel) / (maxAngVel - minAngVel);
 
-			if (maxSpeed > 0) {
-				float speed = (pos - _lastPos).magnitude / Time.deltaTime;
-				speed = (speed - minSpeed) / (maxSpeed - minSpeed);
+            Vector3 pos = refTransform.position;
 
-				if (speed > av) {
-					av = speed;
-				}
-			}
+            if (maxSpeed > 0)
+            {
+                float speed = (pos - _lastPos).magnitude / Time.deltaTime;
+                speed = (speed - minSpeed) / (maxSpeed - minSpeed);
 
-			av = Mathf.Clamp01(av) * maxEffect;
+                if (speed > av)
+                {
+                    av = speed;
+                }
+            }
 
-			_av = Mathf.SmoothDamp(_av, av, ref _avSlew, smoothTime);
+            av = Mathf.Clamp01(av) * maxEffect;
 
-			_m.SetFloat(_propAV, _av);
-			_m.SetFloat(_propFeather, feather);
+            _av = Mathf.SmoothDamp(_av, av, ref _avSlew, smoothTime);
 
-			_lastFwd = fwd;
-			_lastPos = pos;
-		}
+            _m.SetFloat(_propAV, _av);
+            _m.SetFloat(_propFeather, feather);
+            SetCageView();
 
-		void OnRenderImage(RenderTexture src, RenderTexture dest){
-			Graphics.Blit(src, dest, _m);
-		}
+            _lastFwd = fwd;
+            _lastPos = pos;
+        }
 
-		void OnDestroy(){
-			Destroy(_m);
-		}
-		#endregion
-	}
+        void SetCageView()
+        {
+            if (_camera == null)
+            {
+                _camera = Camera.main;
+                return;
+            }
+
+            if (cageSkybox != null)
+            {
+                _m.SetMatrixArray("_EyeToWorld", new Matrix4x4[2]{
+                    _camera.GetStereoViewMatrix(Camera.StereoscopicEye.Left).inverse,
+                    _camera.GetStereoViewMatrix(Camera.StereoscopicEye.Right).inverse
+                });
+
+                Matrix4x4[] eyeProjection = new Matrix4x4[2];
+                eyeProjection[0] = _camera.GetStereoProjectionMatrix(Camera.StereoscopicEye.Left);
+                eyeProjection[1] = _camera.GetStereoProjectionMatrix(Camera.StereoscopicEye.Right);
+                eyeProjection[0] = GL.GetGPUProjectionMatrix(eyeProjection[0], true).inverse;
+                eyeProjection[1] = GL.GetGPUProjectionMatrix(eyeProjection[1], true).inverse;
+                eyeProjection[0][1, 1] *= -1f;
+                eyeProjection[1][1, 1] *= -1f;
+                _m.SetMatrixArray("_EyeProjection", eyeProjection);
+            }
+        }
+
+        void OnRenderImage(RenderTexture src, RenderTexture dest)
+        {
+            Graphics.Blit(src, dest, _m);
+        }
+
+        void OnDestroy()
+        {
+            Destroy(_m);
+        }
+        #endregion
+    }
 }
